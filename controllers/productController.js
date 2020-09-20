6137e4d830b144a1d41cbd4440de283ff22fe39d
teope <46042539+teope@users.noreply.github.com>
2020-09-20 13:23:45 +0800

    Partially Working Add Products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 const productModel = require('../models/Product');
 const { validationResult } = require('express-validator');
+const prodIng = require('../models/productIngredients'); 
 
 //Getting all products
 exports.getAllProducts = (param, callback) =>{
@@ -21,18 +22,27 @@ exports.addProduct = (req, res) => {
   const errors = validationResult(req);
   if (errors.isEmpty())
   {
-    const { prodName, prodPrice } = req.body;
+    const { prodName, category, productPrice } = req.body;
 
     productModel.getOne({ prodName: {$regex: prodName, $options:'i'}}, (err, result) => {
       if (result) {
 				req.flash('error_msg', 'Already have that product. Try again.');
 				res.redirect('/products/add');
-      } else {
-        if(prodName != ""){
+      } 
+
+      else {
+        console.log(prodName);
+        console.log(category);
+        console.log(productPrice);
           var product = {
             prodName: prodName,
-            prodPrice: prodPrice,
-          }
+            category: category,
+            prodPrice: productPrice
+
+           //ingredientID: ingredient,
+            //quantityNeeded: quantity
+           // unitID: unit
+
         }
 
         productModel.add(product, function(err, result){
@@ -43,13 +53,30 @@ exports.addProduct = (req, res) => {
           }
           else {
             console.log("Product added!");
-            res.redirect('/product');
+            req.flash('success_msg', 'Product added!'); 
+            res.redirect('/products');
             console.log(result);
           }
         })
       }
     });
   } else {
+    const messages = errors.array().map((item) => item.msg);
+    req.flash('error_msg', messages.join(' ')); 
     res.redirect('/products/add');
   }
 }; 
+
+exports.getProductName = (param, callback) => {
+  productModel.getName({prodName : true}, (err, ingredients) => {
+    if (err) throw err; 
+
+    const productsObjects = [];
+
+    products.forEach(function(doc) {
+      productsObjects.push(doc.toObject());
+    });
+
+    callback(productsObjects);
+  });
+};
